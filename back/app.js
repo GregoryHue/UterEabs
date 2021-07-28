24562bb8fb7fcf3d250078aa49e836f517bd66fd
BuildTools <unconfigured@null.spigotmc.org>
2021-07-28 15:48:52 +0200

    fcking cors i swear

--- back/app.js
+++ back/app.js
@@ -37,11 +37,11 @@ app.use("/orders", ordersRouter);
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.CROSS_ALLOWED);
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
