6679cda08c75b855e3feb67ee2a2325055dc285d
BuildTools <unconfigured@null.spigotmc.org>
2021-07-28 19:07:01 +0200

    heroku test again2

--- back/controllers/UserAccountController.js
+++ back/controllers/UserAccountController.js
@@ -4,6 +4,8 @@ var jwt = require("jsonwebtoken");
 const mysql_connection = require("../plugins/mysql");
 
 const UserLogin = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   console.log(req.body);
 
   mysql_connection.query(
@@ -77,6 +79,8 @@ const UserLogin = (req, res, next) => {
 };
 
 const UserSignUp = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   bcrypt.hash(req.body.password, 10).then((hash) => {
     console.log(req.body);
 
@@ -120,58 +124,77 @@ const UserSignUp = (req, res, next) => {
   });
 };
 
-
 const UserModify = (req, res, next) => {
-  console.log(req.body)
-  bcrypt.hash(req.body.password, 10).then(
-    (hash) => {
-      var sql = `UPDATE users SET username = ?, password = ?, town = ?, street = ?, zip_code = ? WHERE id = ?;`;
-      connection.query(sql, [req.body.username, hash, req.body.town, req.body.street, req.body.zip_code, req.body.id,], function (err, data) {
-        if (err) {
-          console.log(err)
-          res.json({
-            user: '',
-            message: 'Error 1:' + err,
-          }).status(400)
-        } else {
-          connection.query(`SELECT * FROM users WHERE id = ?`, [req.body.id], function (err, user_data) {
-            if (err) {
-              console.log(err)
-              res.status(400)
-                .json({
-                  message: 'Error 2 :' + err,
-                  user: ''
-                })
-            } else {
-              res.status(200)
-                .json({
-                  message: 'User modified',
-                  user: user_data[0]
-                })
-            }
-          })
-        }
-      });
-    })
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-}
+  console.log(req.body);
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    var sql = `UPDATE users SET username = ?, password = ?, town = ?, street = ?, zip_code = ? WHERE id = ?;`;
+    connection.query(
+      sql,
+      [
+        req.body.username,
+        hash,
+        req.body.town,
+        req.body.street,
+        req.body.zip_code,
+        req.body.id,
+      ],
+      function (err, data) {
+        if (err) {
+          console.log(err);
+          res
+            .json({
+              user: "",
+              message: "Error 1:" + err,
+            })
+            .status(400);
+        } else {
+          connection.query(
+            `SELECT * FROM users WHERE id = ?`,
+            [req.body.id],
+            function (err, user_data) {
+              if (err) {
+                console.log(err);
+                res.status(400).json({
+                  message: "Error 2 :" + err,
+                  user: "",
+                });
+              } else {
+                res.status(200).json({
+                  message: "User modified",
+                  user: user_data[0],
+                });
+              }
+            }
+          );
+        }
+      }
+    );
+  });
+};
 
 const UserDelete = (req, res, next) => {
-  console.log(req.body)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  console.log(req.body);
   var sql = `DELETE FROM users WHERE id = ?;`;
   mysql_connection.query(sql, [req.body.id], function (err, data) {
     if (err) {
-      console.log(err)
-      res.json({
-        message: 'Error 1:' + err,
-      }).status(400)
+      console.log(err);
+      res
+        .json({
+          message: "Error 1:" + err,
+        })
+        .status(400);
     } else {
-      res.json({
-        message: 'User deleted, logging out',
-      }).status(200)
+      res
+        .json({
+          message: "User deleted, logging out",
+        })
+        .status(200);
     }
   });
-
-}
+};
 
 module.exports = { UserLogin, UserSignUp, UserModify, UserDelete };
