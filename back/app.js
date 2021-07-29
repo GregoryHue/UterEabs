2dc720dfb49a5f78cda96225d313038c9c71b93d
BuildTools <unconfigured@null.spigotmc.org>
2021-07-29 20:48:52 +0200

    New commit

--- back/app.js
+++ back/app.js
@@ -29,7 +29,7 @@ app.use(cors())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, XMLHttpRequest ');
     res.header('Access-Control-Allow-Credentials', true);
 
     next();
